7285c6658399e2b51cdc46925374ce60f5624d2a
Piw-Tamon <tamonwan.cc34@gmail.com>
2025-04-11 12:57:52 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Web Development Learner Guide", "Learner", "Writer"], // ใช้ 'strings' ไม่ใช่ 'Strings'
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 900,
+    loop: true
+});
